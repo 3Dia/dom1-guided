7d6fc4d7a4a813dde60ae576d65f422f48ffd3e5
3Dia <tmgantt85@gmail.com>
2019-06-18 22:10:33 -0400

    query selector

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('Connected to the console!');
+
+// DOM- Document Object model (html as an object)
+
+// select stuff from HTML (Elements)
+
+const body = document.querySelector('body');
+console.log(body);
+
+const container = document.querySelector('.container');
+console.log('container',container);
+
+// CLASS
+
+const lambdaButton = document.querySelector('#custom-btn');
+console.log('lambdaButton', lambdaButton);
+
+// const specificSection = document.querySelector('section.secondary-content' h2.services-header')
+
+
+const paragraph = document.querySelector('p');
+console.log(paragraph);
